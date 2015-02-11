00eaba854a8f96ddc5dcbad07d1ebacf6bbaf61a
Vitali Tsevan <vzevan@gmail.com>
2015-02-11 21:55:28 +0300

    closure_linter check

---
+++ lib/plugins/pre_commit/checks/closure_linter.rb
@@ -0,0 +1,26 @@
+require 'pre-commit/checks/plugin'
+
+module PreCommit
+  module Checks
+    class ClosureLinter < Plugin
+      def self.aliases
+        [:closure_linter, :gjslint]
+      end
+
+      def call(staged_files)
+        staged_files = staged_files.grep(/\.js$/)
+        return if staged_files.empty?
+
+        output = `gjslint --nojsdoc #{staged_files.join(" ")}`
+        return if output =~ /(\d*) files checked, no errors found(.*)/
+
+        output
+      end
+
+      def self.description
+        "Runs closure linter syntax check"
+      end
+
+    end
+  end
+end
